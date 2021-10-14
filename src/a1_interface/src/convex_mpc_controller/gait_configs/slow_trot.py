323fc5851cde966d59897f4220bcedc8f1eb3913
Yuxiang Yang <yxyang@berkeley.edu>
2021-10-14 00:02:28 -0700

    Real robot updates

--- src/a1_interface/src/convex_mpc_controller/gait_configs/slow_trot.py
+++ src/a1_interface/src/convex_mpc_controller/gait_configs/slow_trot.py
@@ -16,7 +16,7 @@ def get_config():
   config.mpc_foot_friction = 0.5
   config.mpc_body_mass = 110 / 9.8
   config.mpc_body_inertia = np.array(
-      (0.027, 0, 0, 0, 0.057, 0, 0, 0, 0.064)) * 5.
+      (0.057, 0, 0, 0, 0.057, 0, 0, 0, 0.064)) * 5.
   config.mpc_weight = (1., 1., 0, 0, 0, 10, 0., 0., .1, .1, .1, .0, 0)
 
   # Swing foot settings
