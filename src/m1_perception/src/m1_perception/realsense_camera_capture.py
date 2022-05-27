c548db83d71b06f51a43fb5a4c804357d8eab568
Yuxiang Yang <yxyang@berkeley.edu>
2022-05-26 17:54:03 -0700

    depth working

--- src/m1_perception/src/m1_perception/realsense_camera_capture.py
+++ src/m1_perception/src/m1_perception/realsense_camera_capture.py
@@ -17,21 +17,41 @@ FLAGS = flags.FLAGS
 
 
 def main(_):
+  rospy.init_node('realsense_camera_capture', anonymous=True)
+
   pipeline = rs.pipeline()
   config = rs.config()
-  config.enable_stream(rs.stream.color, 424, 240, rs.format.bgr8, 60)
+  config.enable_stream(rs.stream.color, FLAGS.frame_width, FLAGS.frame_height,
+                       rs.format.bgr8, 60)
+  config.enable_stream(rs.stream.depth, FLAGS.frame_width, FLAGS.frame_height,
+                       rs.format.z16, 60)
   pipeline.start(config)
+  # Align depth to color
+  align = rs.align(rs.stream.color)
 
-  camera_image_publisher = rospy.Publisher(
-      '/camera/color/image_raw', Image, queue_size=1)
-  rospy.init_node('realsense_camera_capture', anonymous=True)
+  color_image_publisher = rospy.Publisher('/camera/color/image_raw',
+                                          Image,
+                                          queue_size=1)
+  depth_image_publisher = rospy.Publisher(
+      '/camera/aligned_depth_to_color/image_raw', Image, queue_size=1)
+
   rate = rospy.Rate(FLAGS.frame_rate)
   while not rospy.is_shutdown():
     frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
+    aligned_frames = align.process(frames)
+
+    aligned_depth_frame = aligned_frames.get_depth_frame()
+    color_frame = aligned_frames.get_color_frame()
+    if not aligned_depth_frame or not color_frame:
+      continue
+
     color_image = np.array(color_frame.get_data())
     msg = ros_numpy.image.numpy_to_image(color_image, 'bgr8')
-    camera_image_publisher.publish(msg)
+    color_image_publisher.publish(msg)
+
+    depth_image = np.array(aligned_depth_frame.get_data())
+    msg = ros_numpy.image.numpy_to_image(depth_image, 'mono16')
+    depth_image_publisher.publish(msg)
     rate.sleep()
   pipeline.stop()
 
