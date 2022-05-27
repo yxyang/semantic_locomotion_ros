12e5cb4e4c1f2c3b9c283d11776df7b71c647569
Yuxiang Yang <yxyang@berkeley.edu>
2022-05-26 22:29:09 -0700

    Clean up, pointcloud working

--- src/info_publisher/src/info_publisher/tf_publisher.py
+++ src/info_publisher/src/info_publisher/tf_publisher.py
@@ -4,12 +4,7 @@
 from absl import app
 from absl import flags
 
-from cv_bridge import CvBridge
-import geometry_msgs.msg
-from image_geometry import PinholeCameraModel
-import numpy as np
-from sensor_msgs.msg import CameraInfo, Image, Imu, PointCloud2
-import ros_numpy
+import geometry_msgs
 import rospy
 import tf2_ros
 
@@ -20,7 +15,7 @@ from a1_interface.msg import robot_state
 FLAGS = flags.FLAGS
 
 CAMERA_POSITION_ROBOT_FRAME = [0.24, 0, 0]
-CAMERA_ORIENTATION_TRANFORM_QUAT = [0.5, -0.5, 0.5, 0.5]
+CAMERA_ORIENTATION_TRANFORM_QUAT = [0.5, -0.5, 0.5, -0.5]
 
 
 class TFPublisher:
@@ -28,12 +23,6 @@ class TFPublisher:
   def __init__(self):
     self.broadcaster = tf2_ros.TransformBroadcaster()
     self._robot_orientation_quat_xyzw = [0, 0, 0, 1]
-    self._camera_model = PinholeCameraModel()
-    self._camera_model.initialized = False
-    self._speed_map_array = None
-    self._cv_bridge = CvBridge()
-    self._pointcloud_publisher = rospy.Publisher(
-        "/perception/speedmap/pointcloud", PointCloud2, queue_size=1)
 
   def robot_state_callback(self, robot_state_data):
     """Publish robot state data to TF."""
@@ -69,121 +58,18 @@ class TFPublisher:
     transform_msg.transform.rotation.w = q[3]
     self.broadcaster.sendTransform(transform_msg)
 
-    # transform_msg = geometry_msgs.msg.TransformStamped()
-    # transform_msg.header.stamp = robot_state_data.timestamp
-    # transform_msg.header.frame_id = "base_link"
-    # transform_msg.child_frame_id = "camera_link"
-    # transform_msg.transform.translation.x = CAMERA_POSITION_ROBOT_FRAME[0]
-    # transform_msg.transform.translation.y = CAMERA_POSITION_ROBOT_FRAME[1]
-    # transform_msg.transform.translation.z = CAMERA_POSITION_ROBOT_FRAME[2]
-    # transform_msg.transform.rotation.x = CAMERA_ORIENTATION_TRANFORM_QUAT[0]
-    # transform_msg.transform.rotation.y = CAMERA_ORIENTATION_TRANFORM_QUAT[1]
-    # transform_msg.transform.rotation.z = CAMERA_ORIENTATION_TRANFORM_QUAT[2]
-    # transform_msg.transform.rotation.w = CAMERA_ORIENTATION_TRANFORM_QUAT[3]
-    # self.broadcaster.sendTransform(transform_msg)
-
-  def camera_imu_callback(self, camera_imu):
-    """Publish camera orientation data to TF."""
-    camera_orientation = camera_imu.orientation
-    camera_orientation_world_frame = [
-        camera_orientation.x, camera_orientation.y, camera_orientation.z,
-        camera_orientation.w
-    ]
-    camera_orientation_rpy = list(
-        p.getEulerFromQuaternion(camera_orientation_world_frame))
-    # Transform between camera and robot coordinate systems
-    _, camera_orientation_world_frame = p.multiplyTransforms(
-        [0., 0., 0.],
-        camera_orientation_world_frame,
-        [0., 0., 0.],
-        CAMERA_ORIENTATION_TRANFORM_QUAT,
-    )
-
-    camera_orientation_rpy = list(
-        p.getEulerFromQuaternion(camera_orientation_world_frame))
-    robot_orientation_rpy = p.getEulerFromQuaternion(
-        self._robot_orientation_quat_xyzw)
-    # Align camera rpy and robot rpy
-    camera_orientation_rpy[2] = robot_orientation_rpy[2]
-    camera_orientation_world_frame = p.getQuaternionFromEuler(
-        camera_orientation_rpy)
-    robot_orientation_world_frame = self._robot_orientation_quat_xyzw
-
-    _, world_orientation_robot_frame = p.invertTransform(
-        [0., 0., 0.], robot_orientation_world_frame)
-    _, camera_orientation_robot_frame = p.multiplyTransforms(
-        [0., 0., 0.], world_orientation_robot_frame, [0., 0., 0.],
-        camera_orientation_world_frame)
-
     transform_msg = geometry_msgs.msg.TransformStamped()
-    transform_msg.header.stamp = camera_imu.header.stamp
+    transform_msg.header.stamp = robot_state_data.timestamp
     transform_msg.header.frame_id = "base_link"
     transform_msg.child_frame_id = "camera_link"
     transform_msg.transform.translation.x = CAMERA_POSITION_ROBOT_FRAME[0]
     transform_msg.transform.translation.y = CAMERA_POSITION_ROBOT_FRAME[1]
     transform_msg.transform.translation.z = CAMERA_POSITION_ROBOT_FRAME[2]
-    transform_msg.transform.rotation.x = camera_orientation_robot_frame[0]
-    transform_msg.transform.rotation.y = camera_orientation_robot_frame[1]
-    transform_msg.transform.rotation.z = camera_orientation_robot_frame[2]
-    transform_msg.transform.rotation.w = camera_orientation_robot_frame[3]
+    transform_msg.transform.rotation.x = CAMERA_ORIENTATION_TRANFORM_QUAT[0]
+    transform_msg.transform.rotation.y = CAMERA_ORIENTATION_TRANFORM_QUAT[1]
+    transform_msg.transform.rotation.z = CAMERA_ORIENTATION_TRANFORM_QUAT[2]
+    transform_msg.transform.rotation.w = CAMERA_ORIENTATION_TRANFORM_QUAT[3]
     self.broadcaster.sendTransform(transform_msg)
-
-  def depth_info_callback(self, camera_info):
-    self._camera_model.fromCameraInfo(camera_info)
-    self._camera_model.initialized = True
-
-  def depth_image_callback(self, image):
-    """Publish pointcloud with speed estimation."""
-    if not self._camera_model.initialized:
-      return
-    projection_matrix = self._camera_model.projectionMatrix()
-    fx, fy = projection_matrix[0, 0], projection_matrix[1, 1]
-    cx, cy = projection_matrix[0, 2], projection_matrix[1, 2]
-    tx, ty = projection_matrix[0, 3], projection_matrix[1, 3]
-
-    depth_image = self._cv_bridge.imgmsg_to_cv2(image,
-                                                desired_encoding="passthrough")
-    depth_array_mm = np.array(depth_image, dtype=np.float32)  # Depth in mm
-    depth_array_m = depth_array_mm / 1000.
-    height, width = image.height, image.width
-    image_coord_y, image_coord_x = np.meshgrid(np.arange(height),
-                                               np.arange(width),
-                                               indexing='ij')
-    ray_x = (image_coord_x - cx - tx) / fx
-    ray_y = (image_coord_y - cy - ty) / fy
-    camera_coord_x = ray_x * depth_array_m
-    camera_coord_y = ray_y * depth_array_m
-    camera_coord_z = depth_array_m
-    if self._speed_map_array is not None:
-      point_cloud_array = np.stack((camera_coord_x, camera_coord_y,
-                                    camera_coord_z, self._speed_map_array),
-                                   axis=-1).astype(np.float32)
-      dtype = np.dtype([('x', point_cloud_array.dtype),
-                        ('y', point_cloud_array.dtype),
-                        ('z', point_cloud_array.dtype),
-                        ('speed', point_cloud_array.dtype)])
-    else:
-      point_cloud_array = np.stack(
-          (camera_coord_x, camera_coord_y, camera_coord_z),
-          axis=-1).astype(np.float32)
-      dtype = np.dtype([('x', point_cloud_array.dtype),
-                        ('y', point_cloud_array.dtype),
-                        ('z', point_cloud_array.dtype)])
-    cloud_msg = PointCloud2()
-    cloud_msg.header.stamp = image.header.stamp
-    cloud_msg.header.frame_id = image.header.frame_id
-    cloud_msg.height = point_cloud_array.shape[0]
-    cloud_msg.width = point_cloud_array.shape[1]
-    cloud_msg.fields = ros_numpy.point_cloud2.dtype_to_fields(dtype)
-    cloud_msg.is_bigendian = False  # assumption
-    cloud_msg.point_step = dtype.itemsize
-    cloud_msg.row_step = cloud_msg.point_step * point_cloud_array.shape[1]
-    cloud_msg.is_dense = np.isfinite(point_cloud_array).all()
-    cloud_msg.data = point_cloud_array.tobytes()
-    self._pointcloud_publisher.publish(cloud_msg)
-
-  def speed_map_callback(self, image):
-    self._speed_map_array = ros_numpy.image.image_to_numpy(image)
 
 
 def main(argv):
@@ -192,13 +78,6 @@ def main(argv):
 
   publisher = TFPublisher()
   rospy.Subscriber("/robot_state", robot_state, publisher.robot_state_callback)
-  rospy.Subscriber("/imu/data", Imu, publisher.camera_imu_callback)
-  rospy.Subscriber("/camera/depth/camera_info", CameraInfo,
-                   publisher.depth_info_callback)
-  rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image,
-                   publisher.depth_image_callback)
-  rospy.Subscriber("/perception/speedmap/image_raw", Image,
-                   publisher.speed_map_callback)
   rospy.spin()
 
 
--- src/m1_perception/src/m1_perception/realsense_camera_capture.py
+++ src/m1_perception/src/m1_perception/realsense_camera_capture.py
@@ -8,11 +8,17 @@ import pyrealsense2 as rs
 import rospy
 
 import ros_numpy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
 
 flags.DEFINE_integer('frame_width', 424, 'frame width.')
 flags.DEFINE_integer('frame_height', 240, 'frame height.')
 flags.DEFINE_integer('frame_rate', 6, 'frame rate.')
+flags.DEFINE_bool('publish_color_image', True,
+                  'whether to publish color image.')
+flags.DEFINE_bool('publish_depth_image', True,
+                  'whether to publish depth image.')
+flags.DEFINE_bool('publish_color_pointcloud', True,
+                  'whether to publish color pointcloud.')
 FLAGS = flags.FLAGS
 
 
@@ -28,31 +34,66 @@ def main(_):
   pipeline.start(config)
   # Align depth to color
   align = rs.align(rs.stream.color)
+  pointcloud = rs.pointcloud()
 
-  color_image_publisher = rospy.Publisher('/camera/color/image_raw',
-                                          Image,
-                                          queue_size=1)
-  depth_image_publisher = rospy.Publisher(
-      '/camera/aligned_depth_to_color/image_raw', Image, queue_size=1)
+  if FLAGS.publish_color_image:
+    color_image_publisher = rospy.Publisher('/camera/color/image_raw',
+                                            Image,
+                                            queue_size=1)
+  if FLAGS.publish_depth_image:
+    depth_image_publisher = rospy.Publisher(
+        '/camera/aligned_depth_to_color/image_raw', Image, queue_size=1)
+
+  if FLAGS.publish_color_pointcloud:
+    color_pointcloud_publisher = rospy.Publisher('/camera/color_pointcloud',
+                                                 PointCloud2,
+                                                 queue_size=1)
 
   rate = rospy.Rate(FLAGS.frame_rate)
   while not rospy.is_shutdown():
     frames = pipeline.wait_for_frames()
     aligned_frames = align.process(frames)
 
-    aligned_depth_frame = aligned_frames.get_depth_frame()
+    depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
-    if not aligned_depth_frame or not color_frame:
+    if not depth_frame or not color_frame:
       continue
 
+    # Publish messages
     color_image = np.array(color_frame.get_data())
-    msg = ros_numpy.image.numpy_to_image(color_image, 'bgr8')
-    color_image_publisher.publish(msg)
+    depth_image = np.array(depth_frame.get_data())
 
-    depth_image = np.array(aligned_depth_frame.get_data())
-    msg = ros_numpy.image.numpy_to_image(depth_image, 'mono16')
-    depth_image_publisher.publish(msg)
+    if FLAGS.publish_color_image:
+      msg = ros_numpy.image.numpy_to_image(color_image, 'bgr8')
+      msg.header.frame_id = 'camera_link'
+      color_image_publisher.publish(msg)
+
+    if FLAGS.publish_depth_image:
+      msg = ros_numpy.image.numpy_to_image(depth_image, 'mono16')
+      msg.header.frame_id = 'camera_link'
+      depth_image_publisher.publish(msg)
+
+    if FLAGS.publish_color_pointcloud:
+      # Process frames
+      points = pointcloud.calculate(depth_frame)
+      pointcloud.map_to(color_frame)
+      vertices = np.asanyarray(points.get_vertices()).view(np.float32).reshape(
+          color_image.shape)
+      dtype = np.dtype({
+          'names': ['b', 'g', 'r', 'x', 'y', 'z'],
+          'formats': [
+              np.float32, np.float32, np.float32, np.float32, np.float32,
+              np.float32
+          ]
+      })
+      arr = np.concatenate((color_image / 255., vertices),
+                           axis=2).astype(np.float32).view(dtype=dtype)
+      msg = ros_numpy.point_cloud2.array_to_pointcloud2(arr,
+                                                        frame_id='camera_link')
+      color_pointcloud_publisher.publish(msg)
+
     rate.sleep()
+
   pipeline.stop()
 
 
