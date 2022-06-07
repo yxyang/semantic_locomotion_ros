a7382f8d3c2950edb7c4bbe29a25545669f0acb9
Yuxiang Yang <yxyang@berkeley.edu>
2022-06-06 21:41:31 -0700

    Yaw bias estimator

--- src/info_publisher/src/info_publisher/tf_publisher.py
+++ src/info_publisher/src/info_publisher/tf_publisher.py
@@ -8,8 +8,6 @@ import geometry_msgs
 import rospy
 import tf2_ros
 
-import pybullet as p
-
 from a1_interface.msg import robot_state
 
 FLAGS = flags.FLAGS
@@ -28,30 +26,13 @@ class TFPublisher:
     """Publish robot state data to TF."""
     transform_msg = geometry_msgs.msg.TransformStamped()
     transform_msg.header.stamp = robot_state_data.timestamp
-    transform_msg.header.frame_id = "base_flat"
+    transform_msg.header.frame_id = "base_footprint"
     transform_msg.child_frame_id = "base_link"
     transform_msg.transform.translation.x = 0.
     transform_msg.transform.translation.y = 0.
     transform_msg.transform.translation.z = robot_state_data.base_position[2]
-    q = robot_state_data.base_orientation_quat_xyzw
+    q = robot_state_data.base_orientation_ground_frame_quat_xyzw
     self._robot_orientation_quat_xyzw = q
-    transform_msg.transform.rotation.x = q[0]
-    transform_msg.transform.rotation.y = q[1]
-    transform_msg.transform.rotation.z = q[2]
-    transform_msg.transform.rotation.w = q[3]
-    self.broadcaster.sendTransform(transform_msg)
-
-    transform_msg = geometry_msgs.msg.TransformStamped()
-    transform_msg.header.stamp = robot_state_data.timestamp
-    transform_msg.header.frame_id = "base_link"
-    transform_msg.child_frame_id = "base_footprint"
-    transform_msg.transform.translation.x = 0.
-    transform_msg.transform.translation.y = 0.
-    base_height = robot_state_data.base_position_ground_frame[
-        2]
-    transform_msg.transform.translation.z = -base_height
-    _, q = p.invertTransform(
-        [0, 0, 0], robot_state_data.base_orientation_ground_frame_quat_xyzw)
     transform_msg.transform.rotation.x = q[0]
     transform_msg.transform.rotation.y = q[1]
     transform_msg.transform.rotation.z = q[2]
